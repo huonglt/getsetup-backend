c2ebe0e430c3d47d5ad3595b6a0a4d556297f24b
helen.le <lthuongvn@yahoo.com>
2022-11-11 21:36:38 +1300

    install nodemon so dont have to restart server after code change

--- src/controllers/guideController.ts
+++ src/controllers/guideController.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from 'express'
+import { request } from 'http'
 
 /**
  * Get guide availability
  */
 export const getGuideAvailability = (req: Request, res: Response) => {
-  res.send('TODO: guide availability')
+  console.log(`request req.params = ${JSON.stringify(req.params)}`)
+
+  res.send('TODO: retrieve guide availability')
 }
--- src/routes/guide.ts
+++ src/routes/guide.ts
@@ -4,8 +4,11 @@ import * as guideController from '../controllers/guideController'
 const router = express.Router()
 
 /**
- * GET /guide/:id request
+ * GET /guide/:id/:week request
+ * Retrieve guide teaching availability for a given week
+ * id: number - guide id
+ * week: number - in range [0-51]
  */
-router.get('/:id', guideController.getGuideAvailability)
+router.get('/:id/:week', guideController.getGuideAvailability)
 
 export default router
