904cec77812dc2e3bf2a689bca68d8630ebc9358
helen.le <lthuongvn@yahoo.com>
2022-11-12 18:16:22 +1300

    validate request data

--- src/controllers/guideController.ts
+++ src/controllers/guideController.ts
@@ -1,10 +1,24 @@
 import { Request, Response } from 'express'
-import { request } from 'http'
+import log from 'loglevel'
+
+import { isValidGuideAvailability } from '../validators/guideValidator'
 
 /**
  * Get guide availability
  */
 export const getGuideAvailability = (req: Request, res: Response) => {
+  const isValid = isValidGuideAvailability(req.params)
+  if (!isValid) {
+    log.error(
+      `Param object is not valid GuideAvailability: ${JSON.stringify(
+        req.params
+      )}`
+    )
+
+    res.status(400).json({ error: 'Data invalid' })
+    return
+  }
+  const { id, week } = req.params
   console.log(`request req.params = ${JSON.stringify(req.params)}`)
 
   res.send('TODO: retrieve guide availability')
