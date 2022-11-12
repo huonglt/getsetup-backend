71b0599bf5072eb0c6b7f6af5846b4e7bd067306
helen.le <lthuongvn@yahoo.com>
2022-11-12 13:18:51 +1300

    change type for guide availability

--- src/models/guide.ts
+++ src/models/guide.ts
@@ -1,25 +1,24 @@
-/**
- * Guide available time: hour.minute. Assume second is 0 for simplicity
- * hour: number from 0 - 23
- * minute: number from 0 - 59
- */
-export type GuideTime = {
-  hour: number
-  minute: number
+enum DAYS {
+  MON = 0,
+  TUS,
+  WED,
+  THUR,
+  FRI,
+  SAT,
+  SUN,
 }
 
 /**
- * Guide availability: example 8.30 - 9.30, 19.00 - 20.00
- * to must be > from
- * can set minimum duration for an available block. ex do not consider a block of only 10 minutes, or a valid block must be at least 15 or 30 mins
+ * For a given week, guide available time in a day: { day: MON, from: 9, to: 10 }
  */
+export type AvailableTime = {
+  day: DAYS
+  from: number
+  to: number
+}
+
 export type GuideAvailability = {
-  from: GuideTime
-  to: GuideTime
-}
-
-export type Guide = {
   user: number
   weekNumber: number
-  availability: GuideAvailability[]
+  availability: AvailableTime[]
 }
