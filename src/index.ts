0819d46016a8f739d1a23f9091e6965e3c24e7ea
helen.le <lthuongvn@yahoo.com>
2022-11-13 20:25:19 +1300

    boostrap app with calls to start expressjs server and mongodb server

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,29 @@
-import express from 'express'
 import dotenv from 'dotenv'
-import { guideRouter } from './routes/index'
+import { startServer } from './app/server'
+import { startMongoDB } from './app/db'
+import log from 'loglevel'
+
+// enable all log level
+log.enableAll()
 
 dotenv.config()
-const port = process.env.PORT
+const {
+  EXPRESS_SERVER_PORT,
+  EXPRESS_SERVER_HOSTNAME,
+  MONGO_DB_HOST,
+  MONG0_DB_NAME,
+  MONG0_DB_PORT,
+} = process.env
 
-const app = express()
-app.use('/guide', guideRouter)
+/**
+ * Bootstrap app by starting up expressjs server and create connection to mongodb
+ */
+const boostrapApp = async () => {
+  // start expressjs server
+  startServer(Number(EXPRESS_SERVER_PORT), EXPRESS_SERVER_HOSTNAME)
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`)
-})
+  // start mongodb
+  startMongoDB(MONGO_DB_HOST, Number(MONG0_DB_PORT), MONG0_DB_NAME)
+}
+
+boostrapApp()
