fb503d25ff929ab2267781ef17b6b3ebe46fbd0e
helen.le <lthuongvn@yahoo.com>
2022-11-13 23:20:12 +1300

    remove AvailableTimeModel cause not needed. add comment

--- src/models/guideModel.ts
+++ src/models/guideModel.ts
@@ -1,23 +1,27 @@
 import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
+/**
+ * Mongoose schema for Available Time, which is a nested document inside GuideAvailability document
+ */
 const availableTimeSchema = new Schema({
   from: { type: Date, required: true },
   to: { type: Date, required: true },
   booked: { type: Boolean, required: true, default: false },
 })
 
+/**
+ * Mongoose schema for Guide Availability
+ */
 const guideAvailabilitySchema = new Schema({
   userId: { type: Number, index: true, required: true, unique: true },
   weekNumber: { type: Number, min: 1, max: 52, required: true, unique: true },
   availability: [availableTimeSchema],
 })
 
-export const AvailableTimeModel = mongoose.model(
-  'AvailableTimeModel',
-  availableTimeSchema
-)
-
+/**
+ * The guide available time model to read and write documents into underlying MongoDB database
+ */
 export const GuideAvailabilityModel = mongoose.model(
   'GuideAvailabilityModel',
   guideAvailabilitySchema
