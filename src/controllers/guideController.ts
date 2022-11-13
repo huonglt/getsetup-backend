f3669fa84b51413e1d19701c7a047cc295584dde
helen.le <lthuongvn@yahoo.com>
2022-11-13 22:53:48 +1300

    use express-validation validationResult to easily return response when request data invalid

--- src/controllers/guideController.ts
+++ src/controllers/guideController.ts
@@ -1,23 +1,19 @@
 import { Request, Response } from 'express'
+import { validationResult } from 'express-validator'
 import log from 'loglevel'
 import { GuideAvailabilityModel } from '../models/guideModel'
-import { isValidGuideAvailability } from '../validators/guideValidator'
 
 /**
  * Retrieve teaching availability for a guide in a given week
  */
 export const getGuideAvailability = async (req: Request, res: Response) => {
   // make sure request data is correct
-  const isValid = await isValidGuideAvailability(req.params)
-  if (!isValid) {
-    log.error(
-      `Param object is not valid GuideAvailability: ${JSON.stringify(
-        req.params
-      )}`
-    )
-
-    res.status(400).json({ error: 'Data invalid' })
-    return
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      success: false,
+      errors: errors.array(),
+    })
   }
 
   // retrieve data from mongodb
@@ -27,5 +23,46 @@ export const getGuideAvailability = async (req: Request, res: Response) => {
     res.status(200).json({ data: result })
   } catch (err) {
     res.status(500).json({ error: 'Some error while trying to fetch data' })
+  }
+}
+
+/**
+ * Add teaching availability timeslot for a given guide in a given week
+ * @param req
+ * @param res
+ */
+export const submitGuideAvailability = async (req: Request, res: Response) => {
+  // validate request data
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      success: false,
+      errors: errors.array(),
+    })
+  }
+
+  // overwrite the availability of a given guide in a given week in mongodb
+  const { userId, weekNumber, availability } = req.body
+  const filter = { userId, weekNumber }
+  const options = { new: true, upsert: true }
+  const guideAvailability = {
+    userId,
+    weekNumber,
+    availability,
+  }
+  try {
+    const result = await GuideAvailabilityModel.findOneAndUpdate(
+      filter,
+      guideAvailability,
+      options
+    )
+    log.info(`submitGuideAvailability: result = ${JSON.stringify(result)}`)
+    res.status(200).json({ data: result })
+    return
+  } catch (err) {
+    res.status(500).json({
+      error:
+        'Some error occurred while trying to save teaching availability data',
+    })
   }
 }
