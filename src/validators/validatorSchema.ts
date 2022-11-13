15829ace91da3ef677fa7aa71e3aabbd27218e68
helen.le <lthuongvn@yahoo.com>
2022-11-13 22:51:17 +1300

    replace yup validation with express-validation so can use it as middleware

---
+++ src/validators/validatorSchema.ts
@@ -0,0 +1,77 @@
+import { checkSchema } from 'express-validator'
+
+/**
+ * Schema to validate data for retrieve teaching availability request
+ */
+export const retrieveGuideAvailabilityRequestSchema = checkSchema({
+  userId: {
+    exists: {
+      errorMessage: 'UserId is required',
+      options: { checkFalsy: true },
+    },
+    isNumeric: { errorMessage: 'UserId must be a number' },
+  },
+  weekNumber: {
+    exists: {
+      errorMessage: 'weekNumber is required',
+      options: { checkFalsy: true },
+    },
+    isNumeric: { errorMessage: 'weekNumber must be a number' },
+  },
+})
+
+/**
+ * Schema to validate data for submitting teaching availability request
+ */
+export const submitGuideAvailabilityRequestSchema = checkSchema({
+  userId: {
+    exists: {
+      errorMessage: 'UserId is required',
+      options: { checkFalsy: true },
+    },
+    isNumeric: { errorMessage: 'UserId must be a number' },
+  },
+  weekNumber: {
+    exists: {
+      errorMessage: 'weekNumber is required',
+      options: { checkFalsy: true },
+    },
+    isNumeric: { errorMessage: 'weekNumber must be a number' },
+  },
+  availability: {
+    exists: {
+      errorMessage: 'availability is required',
+      options: { checkFalsy: true },
+    },
+    isArray: {
+      options: {
+        min: 1,
+      },
+      errorMessage: 'Availability need to be an array',
+    },
+  },
+  'availability.*.from': {
+    exists: {
+      errorMessage: 'From is required',
+      options: { checkFalsy: true },
+    },
+    isDate: {
+      if: (value: string) => {
+        new Date(value).toString() !== 'Invalid Date'
+      },
+      errorMessage: 'from field must be a date string',
+    },
+  },
+  'availability.*.to': {
+    exists: {
+      errorMessage: 'to field is required',
+      options: { checkFalsy: true },
+    },
+    isDate: {
+      if: (value: string) => {
+        new Date(value).toString() !== 'Invalid Date'
+      },
+      errorMessage: 'to field must be a date string',
+    },
+  },
+})
