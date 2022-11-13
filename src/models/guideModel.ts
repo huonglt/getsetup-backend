895f8ed8582f354e854ed43a08da1b387dec2aa2
helen.le <lthuongvn@yahoo.com>
2022-11-13 23:25:14 +1300

    add comment

--- src/models/guideModel.ts
+++ src/models/guideModel.ts
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema
 
 /**
  * Mongoose schema for Available Time, which is a nested document inside GuideAvailability document
+ * Indidate a time slot [from, to] in a particular date where a guide is free or booked
  */
 const availableTimeSchema = new Schema({
   from: { type: Date, required: true },
@@ -12,6 +13,7 @@ const availableTimeSchema = new Schema({
 
 /**
  * Mongoose schema for Guide Availability
+ * Represent schema of teaching availability of guide in a specific week
  */
 const guideAvailabilitySchema = new Schema({
   userId: { type: Number, index: true, required: true, unique: true },
