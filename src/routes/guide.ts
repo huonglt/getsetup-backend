2f36493331bef49ced6f4aca7026e118e7534f1a
helen.le <lthuongvn@yahoo.com>
2022-11-13 22:51:38 +1300

    validation of request data as middleware

--- src/routes/guide.ts
+++ src/routes/guide.ts
@@ -1,5 +1,9 @@
 import express from 'express'
 import * as guideController from '../controllers/guideController'
+import {
+  retrieveGuideAvailabilityRequestSchema,
+  submitGuideAvailabilityRequestSchema,
+} from '../validators/validatorSchema'
 
 const router = express.Router()
 
@@ -9,6 +13,19 @@ const router = express.Router()
  * id: number - guide id
  * week: number - in range [0-51]
  */
-router.get('/:userId/:weekNumber', guideController.getGuideAvailability)
+router.get(
+  '/:userId/:weekNumber',
+  retrieveGuideAvailabilityRequestSchema,
+  guideController.getGuideAvailability
+)
+
+/**
+ * submit availability for a guide in a given week with an array of time available
+ */
+router.post(
+  '/availability',
+  submitGuideAvailabilityRequestSchema,
+  guideController.submitGuideAvailability
+)
 
 export default router
