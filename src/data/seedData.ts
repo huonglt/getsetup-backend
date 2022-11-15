6ddf73d9189f16f9929ed10e357d0dbe3439b222
Helen Le <lthuongvn@yahoo.com>
2022-11-16 09:45:20 +1300

    remove hard coded date date. generate date data instead

--- src/data/seedData.ts
+++ src/data/seedData.ts
@@ -1,3 +1,8 @@
+import {
+  findNextXWeekDays,
+  findWeekDays,
+  findWeekNumber,
+} from '../util/weekUtil'
 /**
  * There is no actual database setup for this excercise
  * This module is to contain seed data, acting like database, for the app
@@ -15,37 +20,27 @@ export const guides = [
   },
 ]
 
-// weekDays for a given week. For now return same set of week days for any week. In the future return better meaningful data
-export const weekDays = [
-  new Date(2022, 10, 14),
-  new Date(2022, 10, 15),
-  new Date(2022, 10, 16),
-  new Date(2022, 10, 17),
-  new Date(2022, 10, 18),
-  new Date(2022, 10, 19),
-  new Date(2022, 10, 20),
-]
+export const generateWeekNumbers = () => {
+  const today = new Date()
+  const currentWeekNumber = findWeekNumber(today)
+  const thisWeekDays = findWeekDays(today)
 
-// list of all week numbers supported by the system
-export const weekNumbers = [
-  {
-    weekNumber: 1,
-    monday: new Date(2022, 10, 14),
-    sunday: new Date(2022, 10, 20),
-  },
-  {
-    weekNumber: 2,
-    monday: new Date(2022, 10, 21),
-    sunday: new Date(2022, 10, 27),
-  },
-  {
-    weekNumber: 3,
-    monday: new Date(2022, 10, 28),
-    sunday: new Date(2022, 11, 4),
-  },
-  {
-    weekNumber: 4,
-    monday: new Date(2022, 11, 5),
-    sunday: new Date(2022, 11, 11),
-  },
-]
+  return [
+    {
+      weekNumber: currentWeekNumber,
+      weekDays: thisWeekDays,
+    },
+    {
+      weekNumber: currentWeekNumber + 1,
+      weekDays: findNextXWeekDays(1),
+    },
+    {
+      weekNumber: currentWeekNumber + 2,
+      weekDays: findNextXWeekDays(2),
+    },
+    {
+      weekNumber: currentWeekNumber + 3,
+      weekDays: findNextXWeekDays(4),
+    },
+  ]
+}
