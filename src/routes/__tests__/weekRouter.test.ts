9829454a4f4d6c2602e45ef124668fb259c656b9
Helen Le <lthuongvn@yahoo.com>
2022-11-16 11:50:10 +1300

    fix unit test after logic change

--- src/routes/__tests__/weekRouter.test.ts
+++ src/routes/__tests__/weekRouter.test.ts
@@ -1,16 +1,18 @@
 import { weekRouter } from '../index'
 import request from 'supertest'
 import express from 'express'
-import { weekNumbers } from '../../data/seedData'
+import { generateAvailabilityWeeks } from '../../data/seedData'
 
 const app = express()
 app.use('/week', weekRouter)
 
 describe('Week router module', () => {
   test('return status and data correctly for request /weekNumbers', async () => {
-    const res = await request(app).get('/week/weekNumbers')
+    const res = await request(app).get('/week/availabilityWeeks')
     expect(res.header['content-type']).toBe('application/json; charset=utf-8')
     expect(res.statusCode).toBe(200)
-    expect(res.text).toEqual(JSON.stringify(weekNumbers))
+
+    const expectedAvailabilityWeeks = generateAvailabilityWeeks()
+    expect(res.text).toEqual(JSON.stringify(expectedAvailabilityWeeks))
   })
 })
