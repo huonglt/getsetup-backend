42eda84fcc8bbbfc4b7bbf72bc0dbbf808e8c31b
Helen Le <lthuongvn@yahoo.com>
2022-11-15 20:57:51 +1300

    add unit test for module retrieveAvailabilityRequestSchema

---
+++ src/validators/__tests__/retrieveAvailabilityRequestSchema.test.ts
@@ -0,0 +1,39 @@
+import { retrieveAvailabilityRequestSchema as schema } from '../retrieveAvailabilityRequestSchema'
+describe('retrieveAvailabilityRequestSchema', () => {
+  it('errors are correct for invalid query', async () => {
+    // userId invalid, weekNumber is missing
+    const results = await Promise.all(
+      schema.map((chain) =>
+        chain.run({
+          query: { userId: null },
+        })
+      )
+    )
+    const userIdError = results[0].context.errors[0]
+    const weekNumberError = results[1].context.errors[0]
+
+    // error UserId is required
+    expect(userIdError.msg).toEqual('UserId is required')
+    expect(userIdError.param).toEqual('userId')
+
+    // error weekNumber is required
+    expect(weekNumberError.msg).toEqual('weekNumber is required')
+    expect(weekNumberError.param).toEqual('weekNumber')
+  })
+
+  it('valid query, no error', async () => {
+    const results = await Promise.all(
+      schema.map((chain) =>
+        chain.run({
+          query: { userId: 1, weekNumber: 1 },
+        })
+      )
+    )
+
+    // no error for field userId
+    expect(results[0].context.errors).toEqual([])
+
+    // no error for field weekNumber
+    expect(results[1].context.errors).toEqual([])
+  })
+})
