71fef6b69db9c6511a9f1066694cdec2aa7cf6d0
Helen Le <lthuongvn@yahoo.com>
2022-11-16 09:45:41 +1300

    generate data so date values are dynamic and correct

--- src/models/weekModel.ts
+++ src/models/weekModel.ts
@@ -1,8 +1,12 @@
-import { weekDays, weekNumbers } from '../data/seedData'
+import { generateWeekNumbers } from '../data/seedData'
+
+const weekNumbers = generateWeekNumbers()
+
 /**
  * Simulate a week model where data is mocked
  */
-const getWeekdays = async () => {
+const getWeekdays = async (weekNumber: number) => {
+  const weekDays = weekNumbers.find((item) => item.weekNumber === weekNumber)
   return Promise.resolve(weekDays)
 }
 
