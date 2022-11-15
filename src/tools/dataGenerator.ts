5f9da8dc5c829bce7bc8a7e37b0074141b7c3970
Helen Le <lthuongvn@yahoo.com>
2022-11-16 11:53:43 +1300

    migrate method generateAvailabilityWeeks to dataGenerator module

---
+++ src/tools/dataGenerator.ts
@@ -0,0 +1,34 @@
+import {
+  findWeekNumber,
+  findWeekDays,
+  findNextXWeekDays,
+} from '../util/weekUtil'
+
+/**
+ * Generate availaiblity weeks for seed data
+ * Assume that the app is currently support 4 availability weeks
+ */
+export const generateAvailabilityWeeks = () => {
+  const today = new Date()
+  const currentWeekNumber = findWeekNumber(today)
+  const thisWeekDays = findWeekDays(today)
+
+  return [
+    {
+      weekNumber: currentWeekNumber,
+      weekDays: thisWeekDays,
+    },
+    {
+      weekNumber: currentWeekNumber + 1,
+      weekDays: findNextXWeekDays(1),
+    },
+    {
+      weekNumber: currentWeekNumber + 2,
+      weekDays: findNextXWeekDays(2),
+    },
+    {
+      weekNumber: currentWeekNumber + 3,
+      weekDays: findNextXWeekDays(4),
+    },
+  ]
+}
