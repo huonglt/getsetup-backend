0ded4703c05e34f786b97cfa3026bb041e1464e5
Helen Le <lthuongvn@yahoo.com>
2022-11-15 18:10:47 +1300

    change weekNumbers seed data to include monday and sunday

--- src/data/seedData.ts
+++ src/data/seedData.ts
@@ -19,4 +19,23 @@ export const weekDays = [
   new Date(2022, 10, 20),
 ]
 
-export const weekNumbers = [1, 2, 3]
+export const weekNumbers = [{
+  weekNumber: 1,
+  monday: new Date(2022, 10, 14),
+  sunday: new Date(2022, 10, 20)
+},
+{
+  weekNumber: 2,
+  monday: new Date(2022, 10, 21),
+  sunday: new Date(2022, 10, 27)
+},
+{
+  weekNumber: 3,
+  monday: new Date(2022, 10, 28),
+  sunday: new Date(2022, 11, 4)
+},
+{
+  weekNumber: 4,
+  monday: new Date(2022, 11, 5),
+  sunday: new Date(2022, 11, 11)
+}]
