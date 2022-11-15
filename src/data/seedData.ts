f48b01e7f651d3d1aaa011cc94b028c396421117
Helen Le <lthuongvn@yahoo.com>
2022-11-15 17:14:48 +1300

    create mock seed data to swap out of mongodb

---
+++ src/data/seedData.ts
@@ -0,0 +1,22 @@
+export const guides = [
+  {
+    userId: 1,
+    userName: 'Doris Wilson',
+  },
+  {
+    userId: 2,
+    userName: 'Amy Smith',
+  },
+]
+
+export const weekDays = [
+  new Date(2022, 10, 14),
+  new Date(2022, 10, 15),
+  new Date(2022, 10, 16),
+  new Date(2022, 10, 17),
+  new Date(2022, 10, 18),
+  new Date(2022, 10, 19),
+  new Date(2022, 10, 20),
+]
+
+export const weekNumbers = [1, 2, 3]
